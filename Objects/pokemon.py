a0cb5839d36b3dd694e346fb2c6952a162580c85
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-02-26 14:54:47 -0300

    [pokemon] Adding repr and eq method.

--- Objects/pokemon.py
+++ Objects/pokemon.py
@@ -8,15 +8,22 @@ class Pokemon():
         self.image_url = image_url
         self.types     = types
 
+    def __eq__(self, other_pokemon):
+        if(self.number == other_pokemon.number):
+            return True
+        return False
+
+    def __repr__(self):
+        return "Pokemon: {0}, Number: {1}".format(self.name, self.number)
+
     @classmethod
     def fill_object_with_json(cls, json):
         pokemons = []
         for pokemon in json:
             try:
-                pokemons.append(Pokemon(pokemon['name'], pokemon['abilities'], pokemon['weakness'], pokemon['number'], pokemon['height'], pokemon['ThumbnailImage'], pokemon['type']))
+                pokemons.append(cls(pokemon['name'], pokemon['abilities'], pokemon['weakness'], pokemon['number'], pokemon['height'], pokemon['ThumbnailImage'], pokemon['type']))
             except:
                 raise
-        
         print("Success")
         return pokemons
     
